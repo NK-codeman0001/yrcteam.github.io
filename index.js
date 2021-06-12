3740e2958c3e24d391012e27ef58ffbfe1009963
Neeraj Kumar <66253834+NK-codeman0001@users.noreply.github.com>
2021-06-13 00:02:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,270 @@
+let data = [
+    {
+        id:1,
+        Position : "YRC Cultural Head 1",
+        Lname: "Upasna",
+        Lpic:"https://drive.google.com/file/d/1_vU0aqyMyhouJjbdsgQVh19zwPpZSAKL/view?usp=sharing",
+        Sword: `hint 1 : I scared to hug people becoz i fear they can count my bones... 
+        hint 2 : Rajma and rice 😂😂`,
+    },
+    {
+        id:2,
+        Position : "YRC Cultural Head 2",
+        Lname: "Laxmi",
+        Lpic:"https://drive.google.com/file/d/18RuyDqAQewxHrB_ZpYvRJhYkl2JBrdez/view?usp=sharing",
+        Sword: `hint 1 : Ms. Syska LED
+        Hint 2: 12 June 2021
+        Baris ki kht pt or tej hwayen, 
+        
+        maano sab kuch apne sath bha Leena chahti thi,
+        
+        Kuch or to nhi bha saki seva meri nind k ,
+        
+        Or shi 03:03 m khedki pr bethi ,
+        
+        Jeth ki phli subh ka swagat kr rhi thi.
+        
+        Mera kamra,
+        Ek dhul ka medan bn chuka tha,
+        
+        Khedki pr tka dadi ka peti kot hwa s kuch ase bteya rha tha
+        
+        Maano brakch bant ki anubhuti s lhra rha ho.
+        
+        Y phli dfa nhi tha ki jeth m bersat hui ho,
+        
+        Khbe thi ki bangal ki khadi m yas tufan aaya h,
+        
+        Ab delhi dur thi to hwayen k jreye apne aane ka ahsas kra hi deya.
+        
+        Lagbhag ghnte bhr bad sb kuch thm sa gya tha,
+        
+        Mgr bhout kuch abhi bhi waaki tha, 
+        hwayen m ek khusboo bnkr, 
+        
+        Mitti ki khusbu ,jo mhj ek mhk nhi blki ek sabut tha,
+        
+        Jmin or aasman k milne ka chupke s
+        Rat ki chhown m,
+        
+        Suraj ki kerno anaja dur .
+        
+        Khi kese gaon m.`,
+    },
+    {
+        id:3,
+        Position : "YRC Cultural Head 3",
+        Lname: "Pooja",
+        Lpic:"https://drive.google.com/open?id=1tOSpR_sEEdkbW6RF8-gZOc_gfj_dimC5",
+        Sword: `Hint 1. Guess what guys?? 
+        We as a family by our art, skills and unique ideas 
+        Creating awareness in youth and world toward health related issues . 
+        I am
+        .
+        .
+         . 
+        . 
+        . 
+        Your new art coordinator guiding, learning and cooperate with all of you.
+        hint2 : very Holy Name`,
+    },
+    {
+      id: 4,
+      Position : "YRC Session head 1",
+      Lname: "Mehak",
+      Lpic:"https://drive.google.com/open?id=1-Z6w08E_-6pdUR7TafejZQtqPf9EGPAx",
+      Sword:`HInt1: Very Nice Anchor
+      HInt 2: From BBA Gang`,
+    },
+    {
+      id: 5,
+      Position : "YRC Session head 2",
+      Lname: "Simran",
+      Lpic:"https://drive.google.com/open?id=1gkQF8a1z1Z8keI_y1H59mD50hfK7drGI",
+      Sword:`Hint 1: Ja ___ -_____, jille apni zindagi`,
+    },
+    {
+      id: 6,
+      Position : "YRC Session head 3",
+      Lname: "Kanupriya",
+      Lpic:"https://drive.google.com/open?id=1h149_nQVsacG5B7WSNcLXCC2bK_WtGAe",
+      Sword:`very uniquiue name meaning Radha(Beloved of Kanha - Lord Krishna) `,
+    },
+    {
+      id: 7,
+      Position : "YRC Social Media Head 1",
+      Lname: "Nishu",
+      Lpic:"https://drive.google.com/file/d/1M4DhKLLWBJ__btkCGmsUom0rbS8iT5tb/view?usp=sharing",
+      Sword:`Frank girl
+      Miss fashioplate Social media coordinator
+      Baato ki dukaan
+      Ms. Refreshment`,
+    },
+    {
+      id: 8,
+      Position : " YRC Social Media Head 2",
+      Lname: "Kritika",
+      Lpic:"https://drive.google.com/open?id=1IjwAyGLJZtxAFsHQU7EQqd7o8digmt-z",
+      Sword:`Haste rehne wali
+      Mere baal😅
+      I m cute😉😛
+      I m innocent
+      I dont get angry on anybody neither i scolds
+      *Malinga*`,
+    },
+    
+    {
+      id: 9,
+      Position : "YRC Designing Head 1",
+      Lname: "Guranshish",
+      Lpic:"https://drive.google.com/open?id=14SGz79fk9ymZRo3yxwH-JvHpaNJgNODh",
+      Sword:`M khud kaise batau 😅😅😂😁`,
+    },
+    
+    {
+      id: 10,
+      Position : "YRC Designing Head 2",
+      Lname: "Jagriti",
+      Lpic:"https://drive.google.com/file/d/116Ab3m25nuEcaJhNQ8IIaf30lAWe3Bdb/view?usp=sharing",
+      Sword:`Hi people
+      I m not gonna tell you who I m but by some line you can guess me 
+      I m that person which 
+      I like to be creative and also love to do craft
+      I m your senior but by seeing me you will never say she is our senior 😁😁
+      l love to do digital arts and also working for designing team
+      I m humble and also so quite person
+      Some of people say me that i m innocent but 😏 they don't know who I am 😁😁
+      I m a black lover🖤
+      Bas aage jisne pehchaan liya usko meri trf se 10 - 10 rupye paytm 😅`,
+    },
+    
+    {
+      id: 11,
+      Position : "YRC Designing Head 3",
+      Lname: "Komal",
+      Lpic:"https://drive.google.com/open?id=1ZS-2x8n8EKruoyOGAPhf8j_aayPrRIF1",
+      Sword:`Ms. Sensitive + ex sgtian`,
+    },
+     
+    {
+      id:12,
+      Position : "YRC Content Head 1",
+      Lname: "Taruni",
+      Lpic:"https://drive.google.com/open?id=1eCfkIqkmgMp4z-55Tz-Mt-Sh4XHTpHer",
+      Sword: "No Hints (❁´◡`❁)",
+    },
+     
+    {
+      id:13,
+      Position : "YRC Content Head 2",
+      Lname: "Eshaan",
+      Lpic:"https://drive.google.com/open?id=1wobo5XqGqCNxCxJ0bfiilNtVtLfS_tlJ",
+      Sword: "No Hints (❁´◡`❁)",
+    },
+     
+    {
+      id:14,
+      Position : "YRC Technical Head 1",
+      Lname: "Elesh",
+      Lpic:"https://drive.google.com/open?id=1jR_GPimV0eMv7eGebdJhtfxCkqi6ZefG",
+      Sword: "No Hints (❁´◡`❁)",
+    },
+     
+    {
+      id:15,
+      Position : "YRC Technical Head 2",
+      Lname: "Kartik",
+      Lpic:"https://drive.google.com/open?id=1v4xWTVNX9VUzMicoiqQf3Z6WqpPegmcv",
+      Sword: "No Hints (❁´◡`❁)",
+    },
+    {
+      id:16,
+      Position : "Jsec1",
+      Lname: "Arpit",
+      Lpic:"https://drive.google.com/open?id=1GwB6DBngGgAzKdsXx8JAPOobFpcBwi9b",
+      Sword: "No Hints (❁´◡`❁)",
+    },
+    
+    {
+        id:17,
+        Position : "Jsec2",
+        Lname: "Sahil",
+        Lpic:"https://drive.google.com/file/d/1M7Ho7zXhDs9FRwLuWLLZAUzaDYw2I3KK/view?usp=sharing",
+        Sword: "No Hints ╰(*°▽°*)╯",
+    },
+    {
+        id:18,
+        Position : "Jsec3",
+        Lname: "Yogita",
+        Lpic:"https://drive.google.com/open?id=1slNQCHvOGglO1SssDM-7DjIxkoqDbrPu",
+        Sword: "No Hints ༼ つ ◕_◕ ༽つ",
+    },
+    {
+      id:19,
+      Position : "Jsec4",
+      Lname: "Manish",
+      Lpic:"https://drive.google.com/open?id=1941qT6qcUM1jNiKQamsTyTlhq4a7t0ZZ",
+      Sword: "No HInts (⊙_⊙;)",
+  },
+]
+
+let viewCards = () =>{
+    let pCards ='';
+    data.map((curValue)=>{
+        let key = curValue.id;
+        pCards += `<div id="expandCard"></div>
+        </div>
+        <div class="card text-center" >
+        <div class="card-body">
+        <button onclick="expandCard(${key})" class="btn btn-primary" type="button"> <h2 class="card-title">${curValue.Position}</h2></button> 
+        
+      </div>`;
+    document.getElementById('dataCards').innerHTML = pCards ;
+    });
+}
+
+let expandCard=(key)=>{
+    data.map((curValue)=>{
+        if(key===curValue.id){
+            let pCard ='';
+            pCard += `<div class="card text-center">
+            <div class="card-body">
+              <h1 class="card-title bg-primary bg-gradient border border-primary rounded-pill ">${curValue.Position}</h1>
+              <div id="guess">
+              <h1 class="card-text border border-primary">${curValue.Sword}</h1>
+              <h1 class="card-text"><small class="text-muted"><input  class="form-control" type="text" value="" placeholder =" Guess the Leaders" id="guessedWord"></small></h1>
+              <button onclick="guess(${key})" class="btn btn-primary" type="button">Guess It</button>
+              </div>
+            </div>
+          </div>`;
+        document.getElementById('expandCard').innerHTML = pCard;
+    
+        }
+})
+} 
+const guess=(key)=>{
+    const inputWord = document.getElementById("guessedWord").value;
+    const name = data[key-1].Lname;
+    const answer = name.toLocaleUpperCase();
+    const guessedWord = inputWord.toLocaleUpperCase();
+    if(guessedWord===answer){
+        let pCard ='';
+            pCard += `<div class="card text-center">
+            <div class="card-body">
+              <h2 class="card-title">Wah Wah Wah moj karde</h2> <br>
+              <img src="https://media.tenor.com/images/d77cae6baa9e2f84f536c5af4034c092/tenor.gif" width=100%" height="100%"  alt="new leaders pic"><br><br><a href=${data[key-1].Lpic} target="_blank">
+              <button class="btn btn-primary" type="button">You Nailed It!!</button></a>
+            </div>
+          </div>`;
+        document.getElementById('guess').innerHTML = pCard;
+    }
+    else{
+        document.getElementById('guess').innerHTML = `<div class="card text-center">
+        <div class="card-body">
+          <h2 class="card-title">KEEP CALM AND Mummy ko bolo Complan pilaye</h2><br>
+          <img src="https://sd.keepcalms.com/i/keep-calm-and-mummy-ko-bolo-complan-pilaye.png" width="50%" height="50%"  alt="new leaders pic">
+        </div>
+      </div>`;;
+    }
+
+}
